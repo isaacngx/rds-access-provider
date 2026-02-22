d619be08a6603234713acff0af08260ee17d065f
Hong-Phat Nguyen <nhphat1997@gmail.com>
2026-02-22 13:59:51 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# RDS Access Provider
+
+This project implements a CloudFormation native resource for managing AWS RDS database access using AWS SSO Permission Sets. It enables automated provisioning and deprovisioning of database user access through CloudFormation templates.
+
+## License
+
+MIT License
---
+++ src/awx_rds_accessprovider/handlers.py
@@ -0,0 +1,109 @@
+import json
+import logging
+from typing import Any, MutableMapping, Optional
+from cloudformation_cli_python_lib import (
+    Action,
+    OperationStatus,
+    ProgressEvent,
+    Resource,
+    SessionProxy,
+    exceptions,
+)
+
+from botocore.exceptions import ClientError
+
+from .models import ResourceHandlerRequest, ResourceModel
+
+# Use this logger to forward log messages to CloudWatch Logs.
+LOG = logging.getLogger(__name__)
+TYPE_NAME = "AWX::RDS::AccessProvider"
+
+resource = Resource(TYPE_NAME, ResourceModel)
+test_entrypoint = resource.test_entrypoint
+
+SSO_INSTANCE_ARN = 'arn:aws:sso:::instance/ssoins-7223b18c2c5ea126'
+
+@resource.handler(Action.CREATE)
+def create_handler(
+    session: Optional[SessionProxy],
+    request: ResourceHandlerRequest,
+    callback_context: MutableMapping[str, Any],
+) -> ProgressEvent:
+    model = request.desiredResourceState
+    ssoClient = session.client("sso-admin", region_name="us-east-1")
+
+    try:
+        response = ssoClient.create_permission_set(
+            Name=f"DB_Access_For_{model.UserName}",
+            InstanceArn=SSO_INSTANCE_ARN
+        )
+        ssoClient.put_inline_policy_to_permission_set(
+            InstanceArn=SSO_INSTANCE_ARN,
+            PermissionSetArn=response['PermissionSet']['PermissionSetArn'],
+            InlinePolicy=json.dumps({
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Effect": "Allow",
+                        "Action": [
+                            "rds-db:connect"
+                        ],
+                        "Resource": [
+                            f"arn:aws:rds-db:ap-southeast-1:891376986941:dbuser:*/{model.UserName}"
+                        ]
+                    }
+                ]
+            })
+        )
+    except ClientError as error:
+        raise exceptions.InternalFailure(f"Failed to create permission set with error {error}")
+    
+    model.PermissionSetArn = response['PermissionSet']['PermissionSetArn']
+
+    return ProgressEvent(
+        message="Successfully created permission set for RDS access",
+        status=OperationStatus.SUCCESS,
+        resourceModel=model,
+    )
+
+# TRADE-OFF
+# We either loop through all permission sets to find the one with the correct name and delete it.
+# Or the ARN of the permission set needs to be part of the model's indentifier.
+@resource.handler(Action.DELETE)
+def delete_handler(
+    session: Optional[SessionProxy],
+    request: ResourceHandlerRequest,
+    callback_context: MutableMapping[str, Any],
+) -> ProgressEvent:
+    model = request.desiredResourceState
+    ssoClient = session.client("sso-admin", region_name="us-east-1")
+    try:
+        ssoClient.delete_permission_set(InstanceArn=SSO_INSTANCE_ARN, PermissionSetArn=model.PermissionSetArn)
+    except ClientError as error:
+        raise exceptions.InternalFailure(f"Failed to delete permission set with error {error}")
+    return ProgressEvent(
+        message="Successfully deleted permission set for RDS access",
+        status=OperationStatus.SUCCESS,
+    )
+
+
+@resource.handler(Action.READ)
+def read_handler(
+    session: Optional[SessionProxy],
+    request: ResourceHandlerRequest,
+    callback_context: MutableMapping[str, Any],
+) -> ProgressEvent:
+    model = request.desiredResourceState
+    ssoClient = session.client("sso-admin", region_name="us-east-1")
+    try:
+        response = ssoClient.describe_permission_set(InstanceArn=SSO_INSTANCE_ARN, PermissionSetArn=model.PermissionSetArn)
+    except ClientError as error:
+        raise exceptions.InternalFailure(f"Failed to read permission set with error {error}")
+
+    model.PermissionSetArn = response['PermissionSet']['PermissionSetArn']
+
+    return ProgressEvent(
+        message="Successfully read permission set for RDS access",
+        status=OperationStatus.SUCCESS,
+        resourceModel=model,
+    )
